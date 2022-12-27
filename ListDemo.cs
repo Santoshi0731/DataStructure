7a4302bb318dfac408c6bcaae4535a69cff39400
Santoshi0731 <santoshipallaye27@gmail.com>
2022-12-27 17:59:48 +0530

    List Demo

---
+++ ListDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class ListDemo
+    {
+        public void ListDisplay()
+        {
+            List<string> list = new List<string>();
+            list.Add("Ramesh");
+            list.Add("Suresh");
+            list.Add("Rita"); 
+            list.Add("Avinash");
+
+            foreach(string ele in list )
+            {
+                Console.WriteLine(ele);
+            }
+
+        }
+
+    }
+}
