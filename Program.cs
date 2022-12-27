315823baea32ebaaf62e59d2b604c1dfdc9fc1c2
Santoshi0731 <santoshipallaye27@gmail.com>
2022-12-27 17:54:10 +0530

    This is Program

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace DataStructure
+{
+   public class Program
+    { 
+        public static void Main(String[] args)
+        {
+            Queue queue = new Queue();
+            queue.DisplayQueue();
+             //Stack stack = new Stack();
+            //stack.stackDisplay();
+            // ListDemo obj=new ListDemo();
+
+            //obj.ListDisplay();
+
+
+
+
+            //    int[] arr = new int[10];
+            //    int[] arr1 = { 1, 2, 3 };
+
+            //    ArrayList obj = new ArrayList();
+            //    obj.Add(1);
+            //    obj.Add("text"); 
+            //    obj.Add(true);
+            //    obj.Add('s');
+
+            //    foreach (var ele in obj)
+            //    {
+            //        Console.WriteLine(ele);
+            //    }
+            //    Console.WriteLine("---------------------------");
+            //    // obj clear();
+
+            //    bool val = obj.Contains(4);
+            //    Console.WriteLine(val);
+            //    Console.WriteLine(obj.Count);
+            //    Console.WriteLine(obj.IndexOf('s'));
+            //    obj.RemoveAt(1);
+            }
+        }
+    }
