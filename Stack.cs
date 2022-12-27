ecd19fa778aa1f582884e72a2c59f47a1ac5f5ce
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2022-12-27 18:10:27 +0530

    Add files via upload

---
+++ Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+   public class Stack
+    {
+        public void stackDisplay()
+        {
+            Stack<int> stack  = new Stack<int>();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+            stack.Push(50);
+            stack.Push(60);
+
+            foreach(int ele in stack)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("Peek"+stack.Peek());
+            Console.WriteLine("-------------------");
+
+            stack.Pop();
+            Console.WriteLine("Peek" + stack.Peek());
+
+            foreach(int ele in stack)
+            { 
+                Console.WriteLine(ele);
+            }
+       }
+    }
+}
